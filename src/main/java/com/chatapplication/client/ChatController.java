b7eced128caf85a5dc648027242fe19c0a5ea4b8
petr.subrt <petr.subrt@student.spsmb.cz>
2022-04-20 14:47:37 +0200

    slight changes

--- src/main/java/com/chatapplication/client/ChatController.java
+++ src/main/java/com/chatapplication/client/ChatController.java
@@ -1,14 +1,27 @@
 package com.chatapplication.client;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class ChatController {
     @FXML
+    public Button ButtonClick;
+    public javafx.scene.control.ListView ListView;
+    public javafx.scene.control.TextField TextField;
     private Label welcomeText;
 
-    @FXML
+
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+
+    }
+
+    public void onButtonClick(ActionEvent actionEvent) {
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.chatapplication.client.chatapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.chatapplication.client to javafx.fxml;
